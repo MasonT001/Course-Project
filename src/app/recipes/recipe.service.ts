1928665c02784df6c42b2771134bda2fcdc161ed
MasonT001 <mltrepagnier@gmail.com>
2022-11-09 18:18:29 -0800

    adding ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,19 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredients.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService { 
     recipeSelected = new EventEmitter<Recipe>();
 
      private recipes: Recipe[] = [
-        new Recipe('A test recipe', 'This is simply a test', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2021/08/chana-masala-recipe-500x500.jpg'),
-        new Recipe('Another test recipe', 'This is a test','https://www.indianhealthyrecipes.com/wp-content/uploads/2021/08/chana-masala-recipe-500x500.jpg' )
+        new Recipe('A test recipe', 'This is simply a test', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2021/08/chana-masala-recipe-500x500.jpg', [ 
+            new Ingredient('Meat', 1),
+            new Ingredient('French fries', 20)
+        ]),
+        new Recipe('Another test recipe', 'This is a test','https://www.indianhealthyrecipes.com/wp-content/uploads/2021/08/chana-masala-recipe-500x500.jpg', [
+            new Ingredient('Buns', 2),
+            new Ingredient('Meat', 1)
+        ] )
       ];
 
       getRecipes() { 
