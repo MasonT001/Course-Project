b885f3bc11e998e0a5ff231a45ac6de7b4b6cf3d
MasonT001 <mltrepagnier@gmail.com>
2023-01-27 09:18:29 -0800

    submitting the recipe edit form

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -3,10 +3,12 @@ import { Ingredient } from "../shared/ingredients.model";
 import { Recipe } from "./recipe.model";
 import { Injectable } from "@angular/core";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
-import { Subject } from "rxjs";
+import { Subject } from "rxjs/Subject";
 
 @Injectable()
 export class RecipeService { 
+
+  recipesChanged = new Subject<Recipe[]>();
 
      private recipes: Recipe[] = [
         new Recipe('A test recipe', 'This is simply a test', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2021/08/chana-masala-recipe-500x500.jpg', [ 
@@ -32,5 +34,14 @@ export class RecipeService {
 
       addIngredientsToShoppingList(ingredients: Ingredient[]) {
         this.slService.addIngredient(ingredients)
+      }
+
+      addRecipe(recpie: Recipe) {
+        this.recipes.push(recpie)
+        this.recipesChanged.next(this.recipes.slice())
+      }
+
+      updateRecipe(index: number, newRecipe: Recipe) { 
+        this.recipes[index] = newRecipe
       }
 }
