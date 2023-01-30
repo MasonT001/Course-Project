0608f567a8b542ec496641ab6e9d265e600f79eb
MasonT001 <mltrepagnier@gmail.com>
2023-01-30 12:24:41 -0800

    updated

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -57,7 +57,7 @@ export class RecipeEditComponent implements OnInit {
           recipeIngredients.push(new FormGroup({
             'name': new FormControl(ingredient.name, Validators.required),
             'amount': new FormControl(ingredient.amount, [Validators.required, Validators.pattern(/^[1-9][0-9]*$/)]
-          }))
+        }))
         }
       }
     }
